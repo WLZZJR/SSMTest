acdc0061830bffcd796eff75d932b6f508843a23
WLZZJR <810552419@qq.com>
2018-03-09 18:04:09 +0800

    更新了service

--- src/main/java/com/wlz/service/impl/BookServiceImpl.java
+++ src/main/java/com/wlz/service/impl/BookServiceImpl.java
@@ -1,10 +1,14 @@
 package com.wlz.service.impl;
 
+import com.wlz.dao.AppointmentDao;
+import com.wlz.dao.BookDao;
 import com.wlz.dto.AppointExecution;
 import com.wlz.entity.Book;
 import com.wlz.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,17 +16,32 @@ public class BookServiceImpl implements BookService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    // 注入Service依赖
+    @Autowired
+    private BookDao bookDao;
+
+    @Autowired
+    private AppointmentDao appointmentDao;
+
+
     @Override
     public Book getById(long bookId) {
-        return null;
+
+        return bookDao.queryById(bookId);
     }
 
     @Override
     public List<Book> getList() {
-        return null;
+        return bookDao.queryAll(0,1000);
     }
 
     @Override
+    @Transactional
+    /**
+     * 使用注解控制事务方法的优点： 1.开发团队达成一致约定，明确标注事务方法的编程风格
+     * 2.保证事务方法的执行时间尽可能短，不要穿插其他网络操作，RPC/HTTP请求或者剥离到事务方法外部
+     * 3.不是所有的方法都需要事务，如只有一条修改操作，只读操作不需要事务控制
+     */
     public AppointExecution appoint(long bookId, long studentId) {
         return null;
     }
